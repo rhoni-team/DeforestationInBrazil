68effae3e61c0b496705ef600554524972014e99
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-13 11:10:13 -0300

    feat(frontend): add static and server configuration to vite config

--- DeforestationInBrazil/frontend/vite.config.ts
+++ DeforestationInBrazil/frontend/vite.config.ts
@@ -1,20 +1,52 @@
-import { fileURLToPath, URL } from 'node:url'
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import VueDevTools from 'vite-plugin-vue-devtools';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
 
+const env = loadEnv("mode", process.cwd(), "");
+const isProduction = env.TARGET_ENV === "production";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), VueDevTools(),],
+  test: {
+    include: ["**/__tests__/**"],
+    exclude: ["**/node_modules/**", "**/dist/**"],
+    environment: "jsdom",
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  root: path.resolve("./src"),
+  base: isProduction ? "" : "/static/",
+  build: {
+    outDir: path.resolve("./static/dist"),
+    assetsDir: "",
+    manifest: true,
+    emptyOutDir: true,
+    target: "es2015",
+    rollupOptions: {
+      input: {
+        main: path.resolve("./src/main.js"),
+      },
+      output: {
+        chunkFileNames: undefined,
+      },
+    },
+  },
+  server: {
+    origin: "http://localhost:3000",
+    host: "localhost",
+    port: 3000,
+    open: false,
+    watch: {
+      usePolling: true,
+      disableGlobbing: false,
+    },
+  },
+});
