91fb5523e4848058b136c68b274243058ee14b01
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-13 11:10:45 -0300

    feat: add static configuration

--- DeforestationInBrazil/DeforestationInBrazil/settings.py
+++ DeforestationInBrazil/DeforestationInBrazil/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "static/"
+DJANGO_VITE_ASSETS_PATH = BASE_DIR / 'frontend' / 'static' / 'dist'
+STATICFILES_DIRS = [
+    DJANGO_VITE_ASSETS_PATH,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
